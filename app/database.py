51ae5af66cc8ea3e5f1feb505139c306426dfbfb
Vanessa Vieira <vanessavieira422@gmail.com>
2024-12-12 15:59:18 -0300

    comentários

--- app/database.py
+++ app/database.py
@@ -2,8 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Caminho para o banco SQLite
 SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
 
+# Configuração do engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+# Criando a base de modelos
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
